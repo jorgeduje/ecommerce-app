8d29e4a3ae1880369f78d8cadf82e18f467fe332
metralleta <ignacio.e.molina@gmail.com>
2023-04-12 15:39:43 -0300

    "Subiendo estructura de carpetas."

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-    return <h1 className="text-3xl font-bold underline text-center text-gray-500 bg-black">Hello world!</h1>
+    return <h1 className="text-3xl font-bold underline text-center text-gray-500">Hello world!</h1>
 }
 
 export default App
